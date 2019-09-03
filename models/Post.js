11b191a7c2421638544e55f9bf8ff3f9e9d68551
unknown <chadbrooks80@gmail.com>
2019-09-03 17:07:52 +0800

    last video: Creating The Post Model

---
+++ models/Post.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    // this references which user created the post and grabs from the user model
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+
+    text: {
+        type: String,
+        required: true
+    },
+
+    name: {
+        Type: String
+    },
+
+    // we can keep the name and avatar even if they delete their profile
+    avatar: {
+        type: String
+    },
+
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'user'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                Type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now()
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+
+})
