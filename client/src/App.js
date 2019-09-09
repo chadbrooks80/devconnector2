98c9c5b1184164f6f09856f907dd8e2a4e1b871f
unknown <chadbrooks80@gmail.com>
2019-09-09 10:18:13 +0800

    Set up User Registration authentication and reload authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react'; // useEffect is the lifcycle functions used in classes
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/layout/NavBar';
@@ -6,26 +6,39 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import { loadUser } from './actions/auth';
+import setAuthtoken from './utils/setAuthToken';
+
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <NavBar />
-        <Route exact path='/' component={Landing} />
-        <section className='container'>
-          <Alert />
-          <Switch>
-            <Route exact path='/register' component={Register} />
-            <Route exact path='/login' component={Login} />
-          </Switch>
-        </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+if (localStorage.token) {
+  setAuthtoken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []); //empty brackets tells it to only load once, as if it is a component did mount, these are set for if updates only if use those updates, learn more!
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <NavBar />
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
